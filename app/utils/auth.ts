5e5a29e6b9b67e5df8639a368eb6a1990c3563e0
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-06-15 23:01:31 +0530

    feat: add Prisma integration for user authentication and database management; update dependencies and .gitignore

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github" 
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "@/lib/db"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GitHub({
       clientId: process.env.GITHUB_CLIENT_ID!,
