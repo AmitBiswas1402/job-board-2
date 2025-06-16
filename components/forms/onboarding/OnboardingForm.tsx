78b79483722d64183a631a197cb015d59113a1e2
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-06-16 15:41:02 +0530

    feat: add Zod schemas for company, job seeker, and job forms
    feat: implement CompanyForm component for onboarding companies
    
    feat: create OnboardingForm component to manage user type selection
    
    feat: add UserTypeSelection component for onboarding user type choice
    
    fix: update LoginForm to redirect to home after Google sign-in
    
    feat: create reusable form components including Form, FormField, and FormMessage
    
    feat: implement custom Select component for location selection
    
    feat: add Textarea component for user input
    
    chore: update ESLint configuration to remove TypeScript extension
    
    chore: update package dependencies including Zod, React Hook Form, and Sonner

---
+++ components/forms/onboarding/OnboardingForm.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { Card, CardContent } from "@/components/ui/card";
+import { useState } from "react";
+import UserTypeSelection from "./UserTypeSelection";
+
+type UserType = "company" | "jobSeeker" | null;
+
+export const OnboardingForm = () => {
+  const [step, setStep] = useState(1);
+  const [userType, setUserType] = useState<UserType>(null);
+
+  const handleUserTypeSelect = (type: UserType) => {
+    setUserType(type);
+    setStep(2);
+  };
+
+  const renderStep = () => {
+    switch (step) {
+      case 1:
+      return <UserTypeSelection onSelect={handleUserTypeSelect} />;
+      case 2:
+        return userType === "company" ? (
+          <p>User is a company</p>
+        ) : (
+          <p>User is a job seeker</p>
+        );
+      // return userType === "company" ? <CompanyForm /> : <JobSeekerForm />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <>
+      <div className="flex items-center gap-3 mb-10">
+        <h1 className="text-4xl font-bold">
+          Job<span className="text-primary"> Find</span>
+        </h1>
+      </div>
+
+      <Card className="w-full max-w-lg">
+        <CardContent className="p-6">{renderStep()}</CardContent>
+      </Card>
+    </>
+  );
+};
