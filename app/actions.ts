96f77d39126d978501883dd1ca0aa917cd055570
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-06-19 23:56:58 +0530

    feat: integrate Arcjet for bot detection and security; add arcjet utility file and update dependencies

--- app/actions.ts
+++ app/actions.ts
@@ -5,6 +5,20 @@ import { companySchema, jobSeekerSchema } from "./utils/zodSchemas";
 import { requireUser } from "./utils/hooks";
 import { prisma } from "@/lib/db";
 import { redirect } from "next/navigation";
+import arcjet, { detectBot, shield } from "@arcjet/next";
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY!,
+  rules: [
+    shield({
+      mode: "LIVE",
+    }),
+    detectBot({
+      mode: "LIVE",
+      allow: [],
+    }),
+  ],
+});
 
 export async function createCompany(data: z.infer<typeof companySchema>) {
   const user = await requireUser();
