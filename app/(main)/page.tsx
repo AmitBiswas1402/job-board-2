9526e5d6895736c31aeb0259fb3850275d9a5aa3
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-06-25 16:23:10 +0530

    feat: implement job filters component with location, job type, and salary range selection; update Home page to include JobFilters feat: add job expiration handling function to manage job status updates; create new functions file chore: add Radix UI checkbox and separator components to the project; update package.json and package-lock.json with new dependencies

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,6 +1,11 @@
+import { JobFilters } from "@/components/general/JobFilters"
+import { Card } from "@/components/ui/card"
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="grid grid-cols-3 gap-8">
+      <JobFilters />
+    </div>
   )
 }
 export default Home
